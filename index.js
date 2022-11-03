c8832f89890234b10551685d0fe44296a8e0c09f
maithili-10 <83817052+maithili-10@users.noreply.github.com>
2022-11-03 10:51:32 +0400

    2 files modified, 1 added

--- index.js
+++ index.js
@@ -4,3 +4,15 @@ import './style.css';
 // Write Javascript code!
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
+
+let bioData={
+
+  myname:"maithili",
+  myAge:"26",
+  getData:function(){
+  console.log(`my name is ${bioData.myname}`)
+  }
+  
+  }
+  
+bioData.getData()
